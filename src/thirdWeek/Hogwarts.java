95f0a88910ac9fb71712f86ef4b8cfbc7148914e
byzaya <byzaya777@gmail.com>
2023-07-05 19:03:47 +0400

    feat: add solution Hogwarts

--- src/thirdWeek/Hogwarts.java
+++ src/thirdWeek/Hogwarts.java
@@ -65,31 +65,53 @@ public class Hogwarts {
     }
 }
 
-//    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        int n = input.nextInt();
-//        int k = input.nextInt();
-//        Integer[] ingr = new Integer[n];
-//        for (int i = 0; i < n; i++) {
-//            ingr[i] = input.nextInt();
-//        }
+
+// решение на python
+
+// INF = int(1e18)
+//MAX_ELEM = int(1e9)
 //
-//        Arrays.sort(ingr, Collections.reverseOrder()); // сортировка по убыванию
 //
-//        ArrayList<Integer> solutions = new ArrayList<>();
-//        for (int i = 0; i < n; i++) {
-//            for (int j = i; j < n; j++) {
-//                if (i == j) {
-//                    solutions.add(ingr[i]);
-//                } else {
-//                    solutions.add(ingr[i] + ingr[j]);
-//                }
-//            }
-//        }
-//        solutions.sort(Collections.reverseOrder());
-//        long solution = 0;
-//        for (int i = 0; i < k; i++) {
-//            solution += solutions.get(i);
-//        }
-//        System.out.println(solution);
-//    }
+//def check(minUs):
+//    ans = 0
+//    cnt = 0
+//    for i in range(1, n + 1):
+//        if usefullness[i] < minUs:
+//            break
+//        cnt += 1
+//        ans += usefullness[i]
+//    j = 2
+//    while j <= n and usefullness[1] + usefullness[j] >= minUs:
+//        j += 1
+//
+//    for i in range(1, n + 1):
+//        if i + 1 >= j:
+//            break
+//        while j - 1 > i and usefullness[i] + usefullness[j - 1] < minUs:
+//            j -= 1
+//        cnt += (j - i - 1)
+//        ans += prefSum[j - 1] - prefSum[i] + usefullness[i] * (j - i - 1)
+//    if cnt >= k:
+//        return ans - (cnt - k) * minUs
+//    else:
+//        return INF
+//
+//
+//n, k = map(int, input().split())
+//usefullness = list(map(int, input().split()))
+//usefullness.sort()
+//usefullness.append(0)
+//usefullness.reverse()
+//prefSum = [0] * (n + 1)
+//for i in range(1, n + 1):
+//    prefSum[i] = prefSum[i - 1] + usefullness[i]
+//l = -MAX_ELEM
+//r = MAX_ELEM
+//while r > l:
+//    m = (l + r + 1) // 2
+//    if check(m) != INF:
+//        l = m
+//    else:
+//        r = m - 1
+//
+//print(check(l))
